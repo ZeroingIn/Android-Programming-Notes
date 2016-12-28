a3c818f839bd6f5ae6d410b6d9fbea27aca21713
Dawns <amnextking@gmail.com>
2016-12-28 08:48:17 +0800

    5.5.1 add CheatActivity.onSaveInstaceState()

--- 2.Android and Model-View-Controller/GeoQuiz/app/src/main/java/com/only/dawns/geoquiz/CheatActivity.java
+++ 2.Android and Model-View-Controller/GeoQuiz/app/src/main/java/com/only/dawns/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,35 +17,55 @@ public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_SHOWN = "com.only.dawns.geoquiz.answer_shown";
     private boolean mAnswerIsTrue;
 
-    public static Intent newIntent(Context packageContext, boolean answerIsTrue){
-        Intent i = new Intent(packageContext,CheatActivity.class);
-        i.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
+    private static final String TAG = "CheatActivity";
+    private static final String IS_ANSWER_SHOWN = "isAnswerShown";
+    private static boolean mIsAnswerShown;
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(IS_ANSWER_SHOWN, mIsAnswerShown);
+    }
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
         return i;
     }
 
-    private void setAnswerShownResult(boolean isAnswerShown){
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        CheatActivity.mIsAnswerShown = isAnswerShown;
         Intent data = new Intent();
-        data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
-        setResult(RESULT_OK,data);
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
     }
 
-    public static boolean wasAnswerShown(Intent result){
-        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        if (savedInstanceState != null) {
+            mIsAnswerShown = savedInstanceState.getBoolean(IS_ANSWER_SHOWN, false);
+            if (mIsAnswerShown) {
+                setAnswerShownResult(true);
+                Log.i(TAG, "mIsAnswerShown:" + mIsAnswerShown);
+            }
 
-        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
+        }
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         Button showAnswer = (Button) findViewById(R.id.showAnswerButton);
         showAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mAnswerIsTrue)
+                if (mAnswerIsTrue)
                     mAnswerTextView.setText(R.string.true_button);
                 else
                     mAnswerTextView.setText(R.string.false_button);
